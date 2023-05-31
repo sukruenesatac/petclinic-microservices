1a111ece0c6a7d046d54d04f1c171fc43e70ea99
sukruenesatac <sukruenesatac@gmail.com>
2023-05-31 19:55:26 +0000

    added jenkins server terraform files

---
+++ infrastructure/msp-9-jenkins-server-template/jenkins.auto.tfvars
@@ -0,0 +1,8 @@
+mykey = "clarus"
+ami = "ami-0889a44b331db0194"
+region = "us-east-1"
+instance_type = "t3a.medium"
+jenkins_server_secgr = "petclinic-jenkins-server-secgr"
+jenkins-server-tag = "Jenkins Server of Petclinic"
+jenkins-profile = "petclinic-jenkins-server-profile"
+jenkins-role = "petclinic-jenkins-server-role"
---
+++ infrastructure/msp-9-jenkins-server-template/jenkinsdata.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+# update os
+dnf update -y
+# set server hostname as jenkins-server
+hostnamectl set-hostname jenkins-server
+# install git
+dnf install git -y
+# install java 11
+dnf install java-11-amazon-corretto -y
+# install jenkins
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+dnf upgrade
+dnf install jenkins -y
+systemctl enable jenkins
+systemctl start jenkins
+# install docker
+dnf install docker -y
+systemctl start docker
+systemctl enable docker
+usermod -a -G docker ec2-user
+usermod -a -G docker jenkins
+# configure docker as cloud agent for jenkins
+cp /lib/systemd/system/docker.service /lib/systemd/system/docker.service.bak
+sed -i 's/^ExecStart=.*/ExecStart=\/usr\/bin\/dockerd -H tcp:\/\/127.0.0.1:2376 -H unix:\/\/\/var\/run\/docker.sock/g' /lib/systemd/system/docker.service
+systemctl daemon-reload
+systemctl restart jenkins
+# install docker compose
+curl -SL https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+# install python 3
+dnf install -y python3-pip python3-devel
+# install ansible
+pip3 install ansible
+# install boto3
+pip3 install boto3 botocore
+# install terraform
+wget https://releases.hashicorp.com/terraform/1.4.6/terraform_1.4.6_linux_amd64.zip
+unzip terraform_1.4.6_linux_amd64.zip -d /usr/local/bin
