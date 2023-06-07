51a42a7da337971489ac181e9bd9f8fd6be486d3
sukruenesatac <sukruenesatac@gmail.com>
2023-06-07 20:07:33 +0000

    added script for creating ECR registry for dev

---
+++ infrastructure/create-ecr-docker-registry-for-dev.sh
@@ -0,0 +1,10 @@
+PATH="$PATH:/usr/local/bin"
+APP_REPO_NAME="clarusway-repo/petclinic-app-dev"
+AWS_REGION="us-east-1"
+
+aws ecr describe-repositories --region ${AWS_REGION} --repository-name ${APP_REPO_NAME} || \
+aws ecr create-repository \
+--repository-name ${APP_REPO_NAME} \
+--image-scanning-configuration scanOnPush=false \
+--image-tag-mutability MUTABLE \
+--region ${AWS_REGION}
